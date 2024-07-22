43cab13d74f2911ff97fde50efc86f0343a48b25
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-07-21 19:48:51 -0600

    Agregar archivos de proyecto.

---
+++ CSharp_Identificar_vocal_cifra_numérica-consonante/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class CharacterIdentifier
+{
+    static void Main()
+    {
+        Console.Write("Ingrese una letra o número: ");
+        char input = Console.ReadLine()[0];
+
+        if (IsNumber(input))
+        {
+            Console.WriteLine("Es un número.");
+        }
+        else if (IsVowel(input))
+        {
+            Console.WriteLine("Es una vocal.");
+        }
+        else if (IsConsonant(input))
+        {
+            Console.WriteLine("Es una consonante.");
+        }
+        else
+        {
+            Console.WriteLine("Carácter no identificado.");
+        }
+    }
+
+    static bool IsNumber(char c) => char.IsDigit(c);
+    static bool IsVowel(char c) => "aeiouAEIOU".IndexOf(c) >= 0;
+    static bool IsConsonant(char c) => char.IsLetter(c) && !IsVowel(c);
+}
